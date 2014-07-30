94b2d7705c8b97e8f46d415ef7c9abea6abaef61
miramzz <mira.muazzez@gmail.com>
2014-07-30 10:22:15 -0700

    data problem solved with help of MIke

--- machine_l.py
+++ machine_l.py
@@ -1,8 +1,7 @@
 from IPython import parallel
 from sklearn.datasets import fetch_20newsgroups_vectorized
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.cross_validation import cross_val_score
+
 
 
 # Create a function which will do a 1-dimensional grid search in parallel.
@@ -17,35 +16,36 @@ from sklearn.cross_validation import cross_val_score
 # This is a much slower classifier, so the parallel computation will
 # be very important!
 
-
-
+def grid_search(alpha):
+    clf = MultinomialNB(alpha)
+    results = numpy.mean(cross_val_score(clf, data.data, data.target))
+    return results
 
 
 
 def create_plot_curve():
-    data = fetch_20newsgroups_vectorized(remove=('headers', 'footers', 'quotes'))
     clients = parallel.Client()
     lview = clients.load_balanced_view()
+    dview = clients[:]
+    dview['data']= fetch_20newsgroups_vectorized(remove=('headers', 'footers', 'quotes'))
     lview.block = True
     alphas = [1E-4, 1E-3, 1E-2, 1E-1]
 
-    def grid_search():
-        results = []
-        for alpha in alphas:
-            clf = MultinomialNB(alpha)
-            results.append(np.mean(cross_val_score(clf, data.data, data.target)))
-        return results
+    with dview.sync_imports():
+        import numpy
+        from sklearn.naive_bayes import MultinomialNB
+        from sklearn.cross_validation import cross_val_score
 
-    res = lview.map(grid_search(), range(10))
+    res = lview.map(grid_search, alphas)
     return res
 
 
 if __name__ == '__main__':
     results = create_plot_curve()
-    best_ = (0.0)
+    best_ = 0
     for res in results:
-        if res[1] > best_[1]:
+        if res > best_:
             best_ = res
-    print "best result", best_[0], best_[1]
+    print "best result", best_
 
 
